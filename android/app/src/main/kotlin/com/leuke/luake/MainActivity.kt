42e2a6ac3889c26f9e25158918c4b7b92cf8fd68
Sushilkumar8847 <skymodfista@gmail.com>
2022-04-14 12:46:22 +0530

    new version

---
+++ android/app/src/main/kotlin/com/leuke/luake/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.leuke.luake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,49 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
+import 'package:camera_web/camera_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:platform_device_id_web/platform_device_id_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:uni_links_web/uni_links_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  AssetsAudioPlayerWebPlugin.registerWith(registrar);
+  CameraPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FlutterFacebookAuthPlugin.registerWith(registrar);
+  FlutterNativeTimezonePlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PlatformDeviceIdWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
+  UniLinksPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
